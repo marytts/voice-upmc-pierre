034477113432289c0eda4fca2858e17d38ffa2c8
Ingmar Steiner <steiner@coli.uni-saarland.de>
2015-02-06 23:04:27 +0100

    update plugin; move voice from build script into custom config file

--- voice.groovy
+++ voice.groovy
@@ -1,5 +1,13 @@
-license {
-    name = 'Creative Commons Attribution-ShareAlike 3.0 Unported'
-    shortName = 'CC-BY-SA'
-    url = 'http://mary.dfki.de/download/by-sa-3.0.html'
+voice {
+    name = 'upmc-pierre'
+    language = 'fr'
+    gender = 'male'
+    type = 'unit selection'
+    description = 'A male French unit selection voice, built at ISIR (UPMC) using data recorded by Pierre Chauvin'
+    license {
+        name = 'Creative Commons Attribution-ShareAlike 3.0 Unported'
+        shortName = 'CC-BY-SA'
+        url = 'http://mary.dfki.de/download/by-sa-3.0.html'
+    }
+    samplingRate = 16000
 }
